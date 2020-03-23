443820bc54c351b04941d466a8334e543d1f1007
Maxim Kugatov <maximkugatov@gmail.com>
2020-03-23 16:08:53 +0200

    chore: added app

---
+++ kafka-rest-proxy-server/src/main/java/com/kugmax/learn/kafka/rest/proxy/server/kafkarestproxyserver/KafkaRestProxyServerApplication.java
@@ -0,0 +1,13 @@
+package com.kugmax.learn.kafka.rest.proxy.server.kafkarestproxyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRestProxyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRestProxyServerApplication.class, args);
+	}
+
+}
